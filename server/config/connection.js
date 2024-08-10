63c566ba202aac08955890b595ba0ff78669c194
juliettengum <juliettengum19@gmail.com>
2024-08-10 03:11:07 +0100

    Initialisation of server, setting up basic structure, with connection,authentication util and installation of packages

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jshopping?compressors=zlib&gssapiServiceName=mongodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
