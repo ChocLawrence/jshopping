b71f6f89eea0611345825de074c468158d18ce54
Juliette Ngum <juliettengum19@gmail.com>
2024-08-20 00:46:08 +0100

    links

--- client/src/components/Visit/index.js
+++ client/src/components/Visit/index.js
@@ -1,24 +1,33 @@
 import React from "react";
 import "./Visit.css";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function Visit() {
   return (
     <div className="visit-container">
       <div className="support">
         <img src="/images/support.png" alt="support" />
-        <p className="product-name">Visit our Support Center</p>
+        <Link to="/support">
+          <p className="product-name">Visit our Support Center</p>
+        </Link>
       </div>
       <div className="order">
         <img src="/images/order.png" alt="order" />
-        <p className="product-name">Check your order status</p>
+        <Link to="/order-status">
+          <p className="product-name">Check your order status</p>
+        </Link>
       </div>
       <div className="shipping">
         <img src="/images/fast.png" alt="shipping" />
-        <p className="product-name">Shipping, Delivery and Store pickup</p>
+        <Link to="/shipping-and-delivery">
+          <p className="product-name">Shipping, Delivery and Store pickup</p>
+        </Link>
       </div>
       <div className="returns">
         <img src="/images/return.png" alt="returns" />
-        <p className="product-name">Returns and exchanges</p>
+        <Link to="/contact">
+          <p className="product-name">Returns and exchanges</p>
+        </Link>
       </div>
     </div>
   );
